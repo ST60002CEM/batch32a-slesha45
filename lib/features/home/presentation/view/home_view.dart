2f22c3538dd426d1110b47b6e61fcfb0bdf2971f
slesha45 <dahalslesha@gmail.com>
2024-06-26 18:07:00 +0545

    Some changes

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:final_assignment/features/home/presentation/view/bottom_view/dashboard_view.dart';
 import 'package:final_assignment/features/home/presentation/view/bottom_view/profile_view.dart';
 import 'package:final_assignment/features/home/presentation/view/bottom_view/setting_view.dart';
-import 'package:final_assignment/features/home/presentation/view/bottom_view/wishlist_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,8 +16,7 @@ class _HomeViewState extends ConsumerState<HomeView> {
   List<Widget> lstScreen = [
     const DashboardView(),
     const ProfileView(),
-    const SettingView(),
-    const WishlistView()
+    const SettingView()
   ];
   @override
   Widget build(BuildContext context) {
@@ -37,10 +35,6 @@ class _HomeViewState extends ConsumerState<HomeView> {
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Setting',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Wishlist',
           ),
         ],
         currentIndex: selectedIndex,
