ad9eb5b0f3e68914825da5cfacacf98825bcbe18
slesha45 <dahalslesha@gmail.com>
2024-06-28 17:48:35 +0545

    presentation updates

--- lib/features/home/presentation/view/bottom_view/dashboard_view.dart
+++ lib/features/home/presentation/view/bottom_view/dashboard_view.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:final_assignment/core/common/show_my_snackbar.dart';
+import 'package:final_assignment/core/common/widgets/my_property_card.dart';
+import 'package:final_assignment/features/home/presentation/viewmodel/property_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -10,138 +13,349 @@ class DashboardView extends ConsumerStatefulWidget {
 }
 
 class _DashboardViewState extends ConsumerState<DashboardView> {
+  final ScrollController _scrollController = ScrollController();
   late bool isDark;
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   void initState() {
-    // isDark = ref.read(isDarkThemeProvider);
     isDark = false;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Estate Ease'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              showMySnackBar(message: 'Refreshing...');
-            },
-            icon: const Icon(
-              Icons.refresh,
-              color: Colors.white,
-            ),
-            // ),
-            // IconButton(
-            //   onPressed: () {
-            //     ref.read(homeViewModelProvider.notifier).logout();
-            //   },
-            //   icon: const Icon(
-            //     Icons.logout,
-            //     color: Colors.white,
-            //   ),
-          ),
-          Switch(
-              value: isDark,
-              onChanged: (value) {
-                setState(() {
-                  isDark = value;
-                  // ref.read(isDarkThemeProvider.notifier).updateTheme(value);
-                });
-              }),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              // Image at the top
-              SizedBox(
-                height: 150,
-                child: Center(child: Image.asset('assets/images/image.png')),
+    final state = ref.watch(propertyViewModelProvider);
+
+    return NotificationListener<ScrollNotification>(
+      onNotification: (notification) {
+        if (notification is ScrollEndNotification &&
+            _scrollController.position.extentAfter == 0) {
+          ref.read(propertyViewModelProvider.notifier).getProperty();
+        }
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Estate Ease'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                showMySnackBar(message: 'Refreshing...');
+              },
+              icon: const Icon(
+                Icons.refresh,
+                color: Colors.white,
               ),
-              const SizedBox(height: 16),
-              // Search bar
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.search),
-                  hintText: 'Search Area or Property ID',
-                  border: OutlineInputBorder(
+            ),
+            Switch(
+                value: isDark,
+                onChanged: (value) {
+                  setState(() {
+                    isDark = value;
+                  });
+                }),
+          ],
+        ),
+        body: SingleChildScrollView(
+          controller: _scrollController,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                // Image at the top
+                SizedBox(
+                  height: 150,
+                  child: Center(child: Image.asset('assets/images/image.png')),
+                ),
+                const SizedBox(height: 16),
+                // Search bar
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: 'Search Area or Property ID',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Category dropdown
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.brown),
                     borderRadius: BorderRadius.circular(8),
                   ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              // Category dropdown
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.brown),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    items: const [
-                      DropdownMenuItem(
-                        value: '1',
-                        child: Text('Apartment'),
-                      ),
-                      DropdownMenuItem(
-                        value: '2',
-                        child: Text('House'),
-                      ),
-                      DropdownMenuItem(
-                        value: '3',
-                        child: Text('Flat'),
-                      ),
-                      DropdownMenuItem(
-                        value: '4',
-                        child: Text('Land'),
-                      ),
-                      DropdownMenuItem(
-                        value: '5',
-                        child: Text('Building'),
-                      ),
-                    ],
-                    onChanged: (value) {},
-                    hint: const Text('Choose a category'),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      items: const [
+                        DropdownMenuItem(
+                          value: '1',
+                          child: Text('Apartment'),
+                        ),
+                        DropdownMenuItem(
+                          value: '2',
+                          child: Text('House'),
+                        ),
+                        DropdownMenuItem(
+                          value: '3',
+                          child: Text('Flat'),
+                        ),
+                        DropdownMenuItem(
+                          value: '4',
+                          child: Text('Land'),
+                        ),
+                        DropdownMenuItem(
+                          value: '5',
+                          child: Text('Building'),
+                        ),
+                      ],
+                      onChanged: (value) {},
+                      hint: const Text('Choose a category'),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              // Look into Estate Ease text
-              Align(
-                alignment: Alignment.centerLeft,
-                child: RichText(
-                  text: const TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Look into ',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black, // Black color
+                const SizedBox(height: 16),
+                // Look into Estate Ease text
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Look into ',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                      TextSpan(
-                        text: 'Estate Ease',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.brown, // Brown color
+                        TextSpan(
+                          text: 'Estate Ease',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.brown,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 16),
+                // Carousel Slider
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 200,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    aspectRatio: 2.0,
+                  ),
+                  items: [
+                    'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                    'https://images.pexels.com/photos/5847577/pexels-photo-5847577.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                    'https://images.pexels.com/photos/6956853/pexels-photo-6956853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                  ].map((imageUrl) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: const BoxDecoration(
+                            color: Colors.amber,
+                          ),
+                          child: Image.network(
+                            imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 16),
+                // Product Grid
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                    childAspectRatio: 0.75,
+                  ),
+                  itemCount: state.property.length,
+                  itemBuilder: (context, index) {
+                    final property = state.property[index];
+                    return MyPropertyCard(property: property);
+                  },
+                ),
+                if (state.isLoading)
+                  const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.red,
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+// import 'package:final_assignment/core/common/show_my_snackbar.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// class DashboardView extends ConsumerStatefulWidget {
+//   const DashboardView({super.key});
+
+//   @override
+//   ConsumerState<DashboardView> createState() => _DashboardViewState();
+// }
+
+// class _DashboardViewState extends ConsumerState<DashboardView> {
+//   late bool isDark;
+//   @override
+//   void initState() {
+//     // isDark = ref.read(isDarkThemeProvider);
+//     isDark = false;
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Estate Ease'),
+//         actions: [
+//           IconButton(
+//             onPressed: () {
+//               showMySnackBar(message: 'Refreshing...');
+//             },
+//             icon: const Icon(
+//               Icons.refresh,
+//               color: Colors.white,
+//             ),
+//             // ),
+//             // IconButton(
+//             //   onPressed: () {
+//             //     ref.read(homeViewModelProvider.notifier).logout();
+//             //   },
+//             //   icon: const Icon(
+//             //     Icons.logout,
+//             //     color: Colors.white,
+//             //   ),
+//           ),
+//           Switch(
+//               value: isDark,
+//               onChanged: (value) {
+//                 setState(() {
+//                   isDark = value;
+//                   // ref.read(isDarkThemeProvider.notifier).updateTheme(value);
+//                 });
+//               }),
+//         ],
+//       ),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Column(
+//             children: [
+//               // Image at the top
+//               SizedBox(
+//                 height: 150,
+//                 child: Center(child: Image.asset('assets/images/image.png')),
+//               ),
+//               const SizedBox(height: 16),
+//               // Search bar
+//               TextField(
+//                 decoration: InputDecoration(
+//                   prefixIcon: const Icon(Icons.search),
+//                   hintText: 'Search Area or Property ID',
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(8),
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 16),
+//               // Category dropdown
+//               Container(
+//                 padding: const EdgeInsets.symmetric(horizontal: 16),
+//                 decoration: BoxDecoration(
+//                   border: Border.all(color: Colors.brown),
+//                   borderRadius: BorderRadius.circular(8),
+//                 ),
+//                 child: DropdownButtonHideUnderline(
+//                   child: DropdownButton<String>(
+//                     isExpanded: true,
+//                     items: const [
+//                       DropdownMenuItem(
+//                         value: '1',
+//                         child: Text('Apartment'),
+//                       ),
+//                       DropdownMenuItem(
+//                         value: '2',
+//                         child: Text('House'),
+//                       ),
+//                       DropdownMenuItem(
+//                         value: '3',
+//                         child: Text('Flat'),
+//                       ),
+//                       DropdownMenuItem(
+//                         value: '4',
+//                         child: Text('Land'),
+//                       ),
+//                       DropdownMenuItem(
+//                         value: '5',
+//                         child: Text('Building'),
+//                       ),
+//                     ],
+//                     onChanged: (value) {},
+//                     hint: const Text('Choose a category'),
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 16),
+//               // Look into Estate Ease text
+//               Align(
+//                 alignment: Alignment.centerLeft,
+//                 child: RichText(
+//                   text: const TextSpan(
+//                     children: [
+//                       TextSpan(
+//                         text: 'Look into ',
+//                         style: TextStyle(
+//                           fontSize: 20,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.black, // Black color
+//                         ),
+//                       ),
+//                       TextSpan(
+//                         text: 'Estate Ease',
+//                         style: TextStyle(
+//                           fontSize: 20,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.brown, // Brown color
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
