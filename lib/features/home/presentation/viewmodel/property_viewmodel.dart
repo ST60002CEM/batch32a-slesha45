1b99d5b470aa263557280f01325272c4ef821a83
slesha45 <dahalslesha@gmail.com>
2024-07-11 18:58:40 +0545

    refresh

--- lib/features/home/presentation/viewmodel/property_viewmodel.dart
+++ lib/features/home/presentation/viewmodel/property_viewmodel.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:final_assignment/features/home/domain/usecases/property_usecase.dart';
 import 'package:final_assignment/features/home/presentation/state/property_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,37 +18,65 @@ class PropertyViewmodel extends StateNotifier<PropertyState> {
   }
 
   final PropertyUsecase propertyUsecase;
+  final Random random = Random();
 
-  Future resetState() async {
+  Future<void> resetState() async {
     state = PropertyState.initial();
-    getProperty();
+    await getProperty(isReset: true);
   }
 
-  Future getProperty() async {
+  Future<void> getProperty({bool isReset=false}) async {
+    if (isReset) {
+      state = state.copyWith(
+        property: [],
+        page: 0,
+        hasReachedMax: false,
+        isLoading: false,
+      );
+    }
+    if (state.isLoading || state.hasReachedMax) return;
+
     state = state.copyWith(isLoading: true);
     final currentState = state;
     final page = currentState.page + 1;
-    final property = currentState.property;
-    final hasReachedMax = currentState.hasReachedMax;
-    if (!hasReachedMax) {
-      // get data from data source
-      final result = await propertyUsecase.pagination(page, 6);
-      result.fold(
-        (failure) =>
-            state = state.copyWith(hasReachedMax: true, isLoading: false),
-        (data) {
-          if (data.isEmpty) {
-            state = state.copyWith(hasReachedMax: true);
-          } else {
-            state = state.copyWith(
-              property: [...property, ...data],
-              page: page,
-              isLoading: false,
-            );
-          }
-        },
-      );
-    }
+
+    final result = await propertyUsecase.pagination(page, 6);
+    result.fold(
+      (failure) => state = state.copyWith(hasReachedMax: true, isLoading: false),
+      (data) {
+        if (data.isEmpty) {
+          state = state.copyWith(hasReachedMax: true);
+        } else {
+          state = state.copyWith(
+            property: [...currentState.property, ...data],
+            page: page,
+            isLoading: false,
+          );
+        }
+      },
+    );
+    
+    // final property = currentState.property;
+    // final hasReachedMax = currentState.hasReachedMax;
+    // if (!hasReachedMax) {
+    //   // get data from data source
+    //   final result = await propertyUsecase.pagination(page, 6);
+    //   result.fold(
+    //     (failure) =>
+    //         state = state.copyWith(hasReachedMax: true, isLoading: false),
+    //     (data) {
+    //       if (data.isEmpty) {
+    //         state = state.copyWith(hasReachedMax: true);
+    //       } else {
+    //         state = state.copyWith(
+    //           property: [...property, ...data],
+    //           page: page,
+    //           isLoading: false,
+    //         );
+    //       }
+    //     },
+    //   );
+    // }
   }
   
 }
